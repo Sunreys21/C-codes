141e5e63cba75611c58ad3f0f6439daa36b28316
Sunreys <jumierreycabanero@gmail.com>
2025-05-18 23:43:15 +0800

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* void prn_message(void)
+{
+	printf("A message for you; ");
+	printf("Have a nice day\n");
+}
+
+int main(void){
+	
+	prn_message(); //inside parenthesis is input
+	
+	
+}*/
+
+
+
